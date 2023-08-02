c6d93c906a5e5b768485613b5ce636d557bcda6d
crypblizz <45455218+crypblizz8@users.noreply.github.com>
2023-08-02 11:45:48 +0200

    chore: remove unnecessary files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,58 +1,53 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+const lightCodeTheme = require('prism-react-renderer/themes/github')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "WalletConnect Docs",
-  tagline:
-    "WalletConnect is an open protocol to communicate securely between Dapps and Wallets.",
-  url: "https://docs.walletconnect.com/",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "throw",
-  favicon: "img/favicon.ico",
-  organizationName: "walletconnect",
-  projectName: "walletconnect-docs",
+  title: 'WalletConnect Docs',
+  tagline: 'WalletConnect is an open protocol to communicate securely between Dapps and Wallets.',
+  url: 'https://docs.walletconnect.com/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'walletconnect',
+  projectName: 'walletconnect-docs',
   scripts: [
     {
-      src: "https://plausible.io/js/plausible.js",
+      src: 'https://plausible.io/js/plausible.js',
       defer: true,
-      "data-domain": "docs.walletconnect.com",
-    },
+      'data-domain': 'docs.walletconnect.com'
+    }
   ],
-
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       {
         docs: {
           breadcrumbs: false,
-          lastVersion: "current",
-          routeBasePath: "/",
-          sidebarPath: require.resolve("./sidebars.js"),
+          lastVersion: 'current',
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
           showLastUpdateTime: true,
-          editUrl:
-            "https://github.com/WalletConnect/walletconnect-docs/blob/main/",
+          editUrl: 'https://github.com/WalletConnect/walletconnect-docs/blob/main/',
           versions: {
             current: {
               badge: false,
-              label: "v2.x",
-              path: "2.0",
-            },
+              label: 'v2.x',
+              path: '2.0'
+            }
           },
-          remarkPlugins: [
-            [require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }],
-          ],
+          remarkPlugins: [[require('@docusaurus/remark-plugin-npm2yarn'), { sync: true }]]
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      },
-    ],
+          customCss: require.resolve('./src/css/custom.css')
+        }
+      }
+    ]
   ],
 
   themeConfig:
@@ -61,100 +56,100 @@ const config = {
       navbar: {
         items: [
           {
-            type: "html",
+            type: 'html',
             value:
-              '<a class="navbar__brand" href="/"><div class="navbar__logo"><img src="/img/walletconnect-logo-white.svg#dark-mode-only"  alt="WalletConnect Logo"><img src="/img/walletconnect-logo-black.svg#light-mode-only"  alt="WalletConnect Logo"></div>WalletConnect<span>Docs<span></a>',
+              '<a class="navbar__brand" href="/"><div class="navbar__logo"><img src="/img/walletconnect-logo-white.svg#dark-mode-only"  alt="WalletConnect Logo"><img src="/img/walletconnect-logo-black.svg#light-mode-only"  alt="WalletConnect Logo"></div>WalletConnect<span>Docs<span></a>'
           },
           {
-            href: "https://github.com/walletconnect/",
-            position: "right",
-            className: "header-github-link",
-            "aria-label": "GitHub repository",
+            href: 'https://github.com/walletconnect/',
+            position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub repository'
           },
 
           {
-            type: "docsVersionDropdown",
-            position: "right",
-          },
-        ],
+            type: 'docsVersionDropdown',
+            position: 'right'
+          }
+        ]
       },
       footer: {
         links: [
           {
-            title: "Docs",
+            title: 'Docs',
             items: [
               {
-                label: "v2.0 Getting Started",
-                to: "/2.0/#getting-started",
+                label: 'v2.0 Getting Started',
+                to: '/2.0/#getting-started'
               },
               {
-                label: "Web3Modal SDK",
-                to: "/2.0/web3modal/about",
+                label: 'Web3Modal SDK',
+                to: '/2.0/web3modal/about'
               },
               {
-                label: "Web3Wallet SDK",
-                to: "/2.0/web3wallet/about",
-              },
-            ],
+                label: 'Web3Wallet SDK',
+                to: '/2.0/web3wallet/about'
+              }
+            ]
           },
           {
-            title: "Community",
+            title: 'Community',
             items: [
               {
-                label: "GitHub Discussions",
-                href: "https://github.com/WalletConnect/walletconnect-monorepo/discussions",
+                label: 'GitHub Discussions',
+                href: 'https://github.com/WalletConnect/walletconnect-monorepo/discussions'
               },
               {
-                label: "Discord",
-                href: "https://discord.com/invite/kdTQHQ6AFQ",
+                label: 'Discord',
+                href: 'https://discord.com/invite/kdTQHQ6AFQ'
               },
               {
-                label: "Twitter",
-                href: "https://twitter.com/walletconnect",
-              },
-            ],
+                label: 'Twitter',
+                href: 'https://twitter.com/walletconnect'
+              }
+            ]
           },
           {
-            title: "More",
+            title: 'More',
             items: [
               {
-                label: "Blog",
-                href: "https://medium.com/walletconnect",
+                label: 'Blog',
+                href: 'https://medium.com/walletconnect'
               },
               {
-                label: "GitHub",
-                href: "https://github.com/walletconnect/walletconnect-docs/",
-              },
-            ],
-          },
+                label: 'GitHub',
+                href: 'https://github.com/walletconnect/walletconnect-docs/'
+              }
+            ]
+          }
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} WalletConnect, Inc.`,
+        copyright: `Copyright © ${new Date().getFullYear()} WalletConnect, Inc.`
       },
       colorMode: {
-        defaultMode: "dark",
+        defaultMode: 'dark',
         disableSwitch: false,
-        respectPrefersColorScheme: false,
+        respectPrefersColorScheme: false
       },
       prism: {
         darkTheme: darkCodeTheme,
         theme: lightCodeTheme,
-        additionalLanguages: ["swift", "kotlin"],
+        additionalLanguages: ['swift', 'kotlin']
       },
       algolia: {
-        appId: "KEO8ND6AUT",
-        apiKey: "5921626237dc9040afc258af25d4e77d",
-        indexName: "walletconnect",
-        contextualSearch: true,
+        appId: 'KEO8ND6AUT',
+        apiKey: '5921626237dc9040afc258af25d4e77d',
+        indexName: 'walletconnect',
+        contextualSearch: true
       },
       announcementBar: {
-        id: "support_us",
+        id: 'support_us',
         content:
           '🚨 v1.0 has been shut down. Wallets and dapps must upgrade to v2.0 immediately. <a rel="noopener noreferrer" href="/2.0/advanced/migration-from-v1.x/overview">Learn more</a>. 🚨',
-        backgroundColor: "#3182ce",
-        textColor: "#fff",
-        isCloseable: true,
-      },
-    },
-};
+        backgroundColor: '#3182ce',
+        textColor: '#fff',
+        isCloseable: true
+      }
+    }
+}
 
-module.exports = config;
+module.exports = config
